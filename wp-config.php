c943edb73b8202416c0b6b20da9cd67ddae13317
iftekharbhuiyan <ib.nadim@gmail.com>
2019-04-27 21:31:12 -0400

    Default Cofiguration file of a WordPress Installation

---
+++ wp-config.php
@@ -0,0 +1,8 @@
+<?php 
+/* That's all, stop editing! Happy blogging. */
+// specify the Number of post revisions
+define('WP_POST_REVISIONS', 3);
+
+// disables post revisions altogether
+define('WP_POST_REVISIONS', false);
+?>
